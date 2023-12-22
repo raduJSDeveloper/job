c3255884fb29fe9631fd9930f9c32c3692937567
Radu <raduJSdeveloper@gmail.com>
2023-12-22 22:35:00 +0200

    radu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const content = [
+  " incepe invatarura 📖",
+  " incepe primul job 💼",
+  " munceste mult 🔑",
+  " inveseste banii cu succes 💰",
+  " mergi in vacanta 👓",
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [step, functiaDinUseState] = useState(1);
+  const [urare, schimbaUrarea] = useState("Munca, si iar munca!");
+  const [apperance, updateVisibility] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) functiaDinUseState(step - 1); 
+    if (step === 5) schimbaUrarea("Munca, si iar munca!");
+  }
+ 
+
+  function handleNext() {
+    if (step < 5) functiaDinUseState(step + 1);
+    if (step === 4) schimbaUrarea("Numai asa reusesti");
+  }
+
+
+
+  return (
+    <> 
+      {apperance && <div className="primaryContainer">
+        <div className="stepsIndexes">
+          {content.map((elem, _, arr) => (
+            <Span etapa={step} text={arr.indexOf(elem) + 1} key={elem} />
+          ))}
+        </div>
+        <span className="middleParagraph">
+          Etapa{step}:{content[step - 1]}
+        </span>
+        <span className="middleParagraph">{urare}</span>
+        <div className="btnContainer">
+
+          
+          <button type="buttonType" onClick={handlePrevious}>
+            Inapoi
+          </button>
+          <button type="buttonType" onClick={handleNext}>
+            Inainte
+          </button>
+        </div>
+      </div>}
+      <button onClick={()=> updateVisibility(!apperance)} className="closeBtn">✕</button>
+    </>
+  );
+}
+
+function Span({ etapa, text }) {
+  console.log(etapa, text);
+  return (
+    <span className={`eachStep ${etapa >= text ? "eachStepActive" : ""}`}>
+      {text}
+    </span>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
